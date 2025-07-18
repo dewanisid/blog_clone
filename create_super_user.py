bc4422263fe4c0e84709256638793d585bc16fff
siddharth dewani <dewanisid@gmail.com>
2025-07-18 11:08:13 +0530

    changes to su create

--- create_super_user.py
+++ create_super_user.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -12,8 +12,12 @@ username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if not User.objects.filter(username=username).exists():
-    print(f"Creating superuser '{username}'...")
-    User.objects.create_superuser(username=username, email=email, password=password)
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        print("Creating superuser...")
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superuser created!")
+    else:
+        print("Superuser already exists.")
 else:
-    print(f"Superuser '{username}' already exists.")
+    print("Environment variables for superuser not found.")
