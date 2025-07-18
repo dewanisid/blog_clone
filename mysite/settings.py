199df9832f58d1794686e70477b8e0fdd43e8501
siddharth dewani <dewanisid@gmail.com>
2025-07-18 09:49:57 +0530

    changes related to postgress

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,6 +16,9 @@ logging.basicConfig(level=logging.DEBUG)
 import os
 import dj_database_url
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = Path(BASE_DIR).joinpath('blog/templates/blog')
